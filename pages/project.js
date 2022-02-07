e210158a6c512a9f02bdb57a5185f13ba7fdea8b
Hannah MacPherson <hannahmacpherson@Hannahs-MacBook-Pro.local>
2022-02-06 19:28:13 -0800

    fixing deployment error

---
+++ pages/project.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+//import components
+
+
+const Container = styled.div`
+  display: flex;
+`
+
+export default function Project() {
+  return (
+    <Container>
+      Projects
+    </Container>
+  )
+}
