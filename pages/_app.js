03b80f4286c078c177949185146afe21041b56de
hannahmak <hannahmakmac@gmail.com>
2022-06-10 18:26:07 -0700

    added title and desc

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import AppProvider from '../utils/provider'
 
 function MyApp({ Component, pageProps }) {
   return <AppProvider>
+    <Head>
+      <title>Hannah MacPherson&apos;s Portfolio</title>
+      <meta name='description' content='Frontend Developer Portfolio'/>
+    </Head>
     <Component {...pageProps} />
     </AppProvider>
 }
